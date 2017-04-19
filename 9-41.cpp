d7f490548046b8442b7bda2f9c4c53b2c0f800c8
b2utyyomi <JG20131426@163.com>
2017-04-19 20:40:13 +0800

    third

---
+++ 9-41.cpp
@@ -0,0 +1,34 @@
+/*******************************
+author: yomi
+date: 17.4.1
+ps: 9-41  Ms--->Miss    Daisy---->AnnaP
+*******************************/
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string greet(string form, string lastname, string title, string::size_type pos, int length)
+{
+    string s = form.replace(form.begin()+8,form.end(), lastname);
+    s = s.replace(5, 2, title, pos, length);
+    return s;
+}
+
+int main()
+{
+    string generic1("Dear Ms Daisy");
+    string generic2("MrsMsMissPeople");
+    string lastname("AnnaP");
+    string Saluto = greet(generic1, lastname, generic2, 5, 4);
+    cout << Saluto << endl;
+    return 0;
+}
+
+/*
+Dear Miss AnnaP
+
+Process returned 0 (0x0)   execution time : 5.304 s
+Press any key to continue.
+
+*/
